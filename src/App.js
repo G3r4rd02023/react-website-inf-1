22646162faa1315bf274230443e28017f6b4786a
Gerardo Lanza <glanza007@gmail.com>
2023-07-06 14:27:03 -0600

    New Carousel and Footer

--- src/App.js
+++ src/App.js
@@ -3,24 +3,33 @@ import Navbar from './components/Navbar';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Carousel from "./Carousel";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Footer from './Footer';
 
 function App() {
-    
-return (
-<>
-<Router>
-<Navbar />
-<Carousel />
-<Routes>
-<Route path='/' />
-</Routes>
-</Router>
-</>
-);
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Carousel autoPlay={true} interval={3000} stopOnHover={true} infiniteLoop={true}>
+          <div>
+            <img src="/images/slide1.jpg" alt="Imagen 1" />
+          </div>
+          <div>
+            <img src="/images/slide2.jpg" alt="Imagen 2" />
+          </div>
+          <div>
+            <img src="/images/slide3.jpg" alt="Imagen 3" />
+          </div>
+        </Carousel>
+        <Footer />
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </Router>
+    </>
+  );
 }
+
 export default App;
-
-
--- src/Carousel.js
+++ src/Carousel.js
@@ -1,53 +1,24 @@
-import React,  { useState, useEffect } from "react";
-import Slider from "react-slick";
-//import Slider from 'react-slider';
-import './Carousel.css';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-const Carousel = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);  
-  const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    };
+export default function Carousel() {
+  return (
+    <Carousel autoPlay={true} interval={3000}> {/* intervalo de tiempo en milisegundos */}
+  <div>
+  <img src="/images/slide1.jpg" alt="Imagen 1" />
+  </div>
+  <div>
+  <img src="/images/slide2.jpg" alt="Imagen 2" />
+  </div>
+  <div>
+  <img src="/images/slide3.jpg" alt="Imagen 3" />
+  </div>
+    </Carousel>
+  );
+}
+
+
   
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % totalSlides);
-      }, 3000);
-  
-      return () => clearInterval(interval);
-    }, []);
 
-    const totalSlides = 3; // Total de diapositivas en el carrusel
-
-  const handleSlideChange = (index) => {
-    setCurrentIndex(index);
-  };
-
-    return (
-      
-      <div className="carousel-container"> {/* Agrega la clase CSS al contenedor */}
-      <Slider {...settings}  value={currentIndex}
-      onChange={handleSlideChange}
-      min={0}
-      max={totalSlides - 1}
-      >
-       <div>
-        <img src="/images/slide1.jpg" alt="Imagen 1" />
-      </div>
-        <div>
-        <img src="/images/slide2.jpg" alt="Imagen 2" />
-        </div>
-        <div>
-        <img src="/images/slide3.jpg" alt="Imagen 3" />
-        </div>
-      </Slider>
-      </div>
-    ) ;
-  };
-  
-  export default Carousel;
+ 
   
